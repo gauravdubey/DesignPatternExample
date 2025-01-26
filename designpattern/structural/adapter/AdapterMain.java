66863586a1478d6f0f4e839f380f6f196b4f8cde
GAURAV DUBEY <i.gauravdubey@gmail.com>
2025-01-27 02:49:06 +0530

    Add files via upload
    Added Adapter Main class

---
+++ designpattern/structural/adapter/AdapterMain.java
@@ -0,0 +1,8 @@
+package com.gaurav.designpattern.structural.adapter;
+
+public class AdapterMain {
+    public static void main(String[] args) {
+        System.out.println("Adapter Design Pattern : ");
+        
+    }
+}
